b2e2324b56102352d79c49bf631206be70cf499c
Bmalkes <bmalkes@gmail.com>
2021-04-06 10:09:42 +0300

    push1

---
+++ script.js
@@ -0,0 +1,11 @@
+new Glider(document.querySelector('.glider'), {
+    slidesToShow:3,
+    slidesToScroll: 1,
+    itemWidth:400,
+    exactWidth:false,
+    draggable: true,
+    dots: '.dots',
+
+  });
+
+ 
